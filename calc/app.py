3e36856e9f8a20e4cbd2e9ecd716c1232ef4bb73
Jeremy Warren <j3warren@gmail.com>
2022-08-16 10:45:09 -0400

    add cache files

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def add():
+    """returns sum of a, b"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"{a+b}"
+
+
+@app.route('/sub')
+def sub():
+    """returns difference of a, b"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"{a-b}"
+
+
+@app.route('/mult')
+def mult():
+    """returns product of a, b"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"{a*b}"
+
+
+@app.route('/div')
+def div():
+    """returns quotient of a, b"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"{a/b}"
